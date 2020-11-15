9c0b901a6506aaa0695bbed3234fe319dfef6e0b
Nissa0620 <issayori924@gmail.com>
2020-11-15 16:20:13 +0900

    user registration

--- useful/resources/views/welcome.blade.php
+++ useful/resources/views/welcome.blade.php
@@ -4,10 +4,8 @@
     <div class="center jumbotron mt-5">
         <div class="text-center">
             <h1>知恵の共有場所<br>USEFULへようこそ！</h1>
-        <div class="mt-5">
-            <button type="button" class="btn btn-primary mr-4">登録</button>
-            <button type="button" class="btn btn-primary ml-4">ログイン</button>
-        </div>
+            {{-- ユーザー登録ページへのリンク --}}
+            {!! link_to_route('signup.get', 'Sign up now!', [], ['class' => 'btn btn-lg btn-primary']) !!}
         </div>
     </div>
 @endsection
